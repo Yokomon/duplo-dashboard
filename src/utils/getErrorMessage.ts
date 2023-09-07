a028a9ebb9b9649f49b589cf293ebb78faacc3b2
yokomon <marowmars@gmail.com>
2023-09-07 22:43:15 +0100

    Helper: getErrorMessage

---
+++ src/utils/getErrorMessage.ts
@@ -0,0 +1,6 @@
+export const getErrorMessage: {
+  [T: string]: string;
+} = {
+  "auth/user-not-found": "Invalid credentials",
+  "auth/email-already-in-use": "Email is already registered. Please login!",
+};
